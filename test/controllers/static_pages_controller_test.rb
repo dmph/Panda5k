62e331145cf01125bc7bd838a5473ed2ce30fbb3
Chris Gordon <cmgordonf31@gmail.com>
2015-11-27 20:06:15 +0000

    Finish static page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,22 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | The Panda Stroll"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | The Panda Stroll"
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | The Panda Stroll"
+  end
 end
